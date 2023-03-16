040ac547487c255870f656b7c902e3f373cb0c04
Khetha100 <Khethukuthula990@gmail.com>
2023-03-16 08:46:44 +0200

    Adding pieces and some pieces class code

--- src/main/java/Chess/Pieces.java
+++ src/main/java/Chess/Pieces.java
@@ -1,5 +1,22 @@
 package Chess;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
 public class Pieces {
+    public static BufferedImage image;
+//    BufferedImage image = null;
+    public static void main(String[] args){
+        image = ImageIO.read(new File(one_each.png));
+        int rows = 6;
+        int columns = 2;
+        BufferedImage img[] = new BufferedImage[32];
+        int sub_image_width = image.getWidth();
+
+    }
+
+
 
 }
