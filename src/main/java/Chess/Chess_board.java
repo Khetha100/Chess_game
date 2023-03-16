c9bd0b3819415727a51c59f4427cd0dfdc84cf0e
Khetha100 <Khethukuthula990@gmail.com>
2023-03-16 07:25:44 +0200

    Making chess board fit by removing the boarders and making squares darker

--- src/main/java/Chess/Chess_board.java
+++ src/main/java/Chess/Chess_board.java
@@ -9,6 +9,7 @@ public class Chess_board {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 512, 512);
+        frame.setUndecorated(true);
         JPanel panel = new JPanel(){
             @Override
             public void paint(Graphics g){
@@ -17,10 +18,10 @@ public class Chess_board {
                 for (int i = 0;i<8;i++){
                    for (int j = 0; j<8;j++){
                         if (white) {
-                            g.setColor(Color.white);
+                            g.setColor(Color.white.darker());
                         }
                         else{
-                            g.setColor(Color.black);
+                            g.setColor(Color.black.darker());
                         }
                         g.fillRect(i*64,j*64,64,64);
                         white=!white;
@@ -32,7 +33,7 @@ public class Chess_board {
 
         };
         frame.add(panel);
-        frame.setDefaultCloseOperation(2);
+        frame.setDefaultCloseOperation(3);
         frame.setVisible(true);
 
     }
